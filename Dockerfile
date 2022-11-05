de06454bb1b1a9e930b7b5be497a78f31cc847a2
Dave Johnson <snoopdave@gmail.com>
2022-11-05 18:38:49 -0400

    ongoing dev...

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:16
 
 WORKDIR /usr/src/app
-COPY ../server/package*.json ./
+COPY package*.json ./
 RUN npm install
-COPY ../server/* .
+COPY server/* .
 EXPOSE 4000
 CMD [ "npm", "run", "start" ]
--- deploy/local/nginx-controller-install.sh
+++ deploy/local/nginx-controller-install.sh
@@ -3,6 +3,6 @@ helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 kubectl create namespace ingress-nginx
 kubens ingress-nginx
-helm install stable/nginx-ingress --name nginx \
+helm install stable/nginx-ingress nginx \
     --set controller.service.type=LoadBalancer,rbac.create=true,controller.extraArgs.enable-ssl-passthrough=""
 
