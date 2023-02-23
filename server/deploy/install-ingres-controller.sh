18bca22e3bfde66b6da61484f0e15548c59437e6
David M. Johnson <snoopdave@users.noreply.github.com>
2023-02-23 15:15:20 -0500

    Progress towards a Helm chart for Postgres, server and client. (#19)

--- server/deploy/install-ingres-controller.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-kubectl create namespace ingress-nginx
-kubens namespace ingress-nginx
-helm install stable/nginx-ingress --name nginx \
-    --set controller.service.type=LoadBalancer,rbac.create=true,controller.extraArgs.enable-ssl-passthrough=""
-
