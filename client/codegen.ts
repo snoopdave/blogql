b50b6f876b7f51ea6b418207b99b892e9a4a5f3d
David M. Johnson <snoopdave@gmail.com>
2024-06-22 08:51:32 -0400

    Correct path to schema

--- client/codegen.ts
+++ client/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "../server/schema.graphql",
+  schema: "./schema.graphql",
   documents: "src/graphql",
   generates: {
     "src/gql/": {
