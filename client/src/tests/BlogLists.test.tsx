aa72c177394c86b3806880b015bdaec3df81dc74
David M. Johnson <snoopdave@gmail.com>
2024-07-05 18:53:51 -0400

    test fixes

--- client/src/tests/BlogLists.test.tsx
+++ client/src/tests/BlogLists.test.tsx
@@ -1,30 +1,36 @@
+import { jsx as _jsx } from "react/jsx-runtime";
 /**
  * Copyright David M. Johnson (snoopdave@gmail.com).
  * Licensed under Apache Software License v2.
  */
-
-import '@testing-library/jest-dom'
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import '@testing-library/react';
-import {BlogsList} from '../blogs/BlogsList';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {screen, render} from '@testing-library/react';
-import {Routes, Route} from "react-router";
-import {ApolloProvider} from '@apollo/client';
-import {client} from '../setupTests';
-import React from "react";
+import { BlogsList } from '../blogs/BlogsList';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import { Routes, Route } from "react-router";
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../setupTests';
 import './MatchMediaMock';
+import { act } from 'react';
+import {createRoot} from "react-dom/client"; // Import act from react
 
 it('BlogsList renders without error with Mocked Service Worker', async () => {
-    const render1 = render(
-        <ApolloProvider client={client}>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<BlogsList />} />
-                </Routes>
-            </Router>
-        </ApolloProvider>
-    );
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+    const root = createRoot(container);
+    await act(async () => {
+        root.render(
+            <ApolloProvider client={client}>
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<BlogsList />} />
+                    </Routes>
+                </Router>
+            </ApolloProvider>
+        );
+    });
+
     expect(await screen.findByText('Blog 1')).toBeInTheDocument();
     expect(await screen.findByText('Blog 2')).toBeInTheDocument();
 });
